8a8dd496b565a9d63859916e6972fb720eabe496
ryan-rsm-mckenzie <ryan__mckenzie@hotmail.com>
2019-07-25 22:07:36 -0700

    Update Settings.cpp

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -5,14 +5,14 @@
 
 bool Settings::loadSettings(bool a_dumpParse)
 {
-	return Json2Settings::Settings::loadSettings(FILE_NAME, false, a_dumpParse);
+	return Json2Settings::Settings::loadSettings(FILE_NAME, a_dumpParse);
 }
 
 
-decltype(Settings::constructibleObjectMenuPatch)	Settings::constructibleObjectMenuPatch("constructibleObjectMenuPatch", false, true);
-decltype(Settings::alchemyMenuPatch)				Settings::alchemyMenuPatch("alchemyMenuPatch", false, true);
-decltype(Settings::smithingMenuPatch)				Settings::smithingMenuPatch("smithingMenuPatch", false, true);
-decltype(Settings::enchantmentLearnedPatch)			Settings::enchantmentLearnedPatch("enchantmentLearnedPatch", false, false);
-decltype(Settings::enchantmentCraftedPatch)			Settings::enchantmentCraftedPatch("enchantmentCraftedPatch", false, false);
-decltype(Settings::enchantingMenuExitPatch)			Settings::enchantingMenuExitPatch("enchantingMenuExitPatch", false, true);
-decltype(Settings::poisonPatch)						Settings::poisonPatch("poisonPatch", false, true);
+decltype(Settings::constructibleObjectMenuPatch)	Settings::constructibleObjectMenuPatch("constructibleObjectMenuPatch", true);
+decltype(Settings::alchemyMenuPatch)				Settings::alchemyMenuPatch("alchemyMenuPatch", true);
+decltype(Settings::smithingMenuPatch)				Settings::smithingMenuPatch("smithingMenuPatch", true);
+decltype(Settings::enchantmentLearnedPatch)			Settings::enchantmentLearnedPatch("enchantmentLearnedPatch", false);
+decltype(Settings::enchantmentCraftedPatch)			Settings::enchantmentCraftedPatch("enchantmentCraftedPatch", false);
+decltype(Settings::enchantingMenuExitPatch)			Settings::enchantingMenuExitPatch("enchantingMenuExitPatch", true);
+decltype(Settings::poisonPatch)						Settings::poisonPatch("poisonPatch", true);
